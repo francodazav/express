7df607b8d3a54268e60c1a6fb27ff1c77b63434c
francodazav <32213325+francodazav@users.noreply.github.com>
2025-05-18 23:06:10 -0400

    cambie la ruta de movies

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const crypto = require("node:crypto");
-const movies = require("../movies.json");
+const movies = require("./movies.json");
 const { validateMovie, validatePartialMovie } = require("./movieScheme");
 const app = express();
 
