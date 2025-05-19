cf487fabf7682e93efa3c184417ba0814d5e7490
francodazav <32213325+francodazav@users.noreply.github.com>
2025-05-18 21:56:07 -0400

    Cambie port a PORT

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.disable("x-powered-by");
 
-const PORT = process.env.port ?? 3000;
+const PORT = process.env.PORT ?? 3000;
 
 const ACEPTEP_ORIGINS = [
   "http://localhost:3000",
